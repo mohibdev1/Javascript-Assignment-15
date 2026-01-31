0e90c24a3b4c0a42aa836a6539d0ab2d64dac01b
Mohib ullah <>
2026-01-31 20:49:17 +0500

    update

--- app.js
+++ app.js
@@ -1,30 +1,42 @@
 
-
-//  === todo app ====
-
 function addTodo(){
     var input = document.getElementById("todo")
     var list = document.getElementById("list")
-    // console.log(input.value);
-    var todo = document.createTextNode(input.value)
+
     var li = document.createElement("li")
-    li.appendChild(todo)
+    var todoText = document.createTextNode(input.value)
+    li.appendChild(todoText)
+
     var deleteBtn = document.createElement("button")
     deleteBtn.setAttribute("onclick", "delTodo()")
+    deleteBtn.appendChild(document.createTextNode("Delete"))
+
     var editBtn = document.createElement("button")
-    var delBtnText = document.createTextNode("Delete")
-    var editBtnText = document.createTextNode("Edit")
-    deleteBtn.appendChild(delBtnText)
-    editBtn.appendChild(editBtnText)
+    editBtn.setAttribute("onclick", "editTodo(this)")
+    editBtn.appendChild(document.createTextNode("Edit"))
+
     li.appendChild(deleteBtn)
     li.appendChild(editBtn)
-    list.after(li)
+
+    list.appendChild(li)
     input.value = ""
 }
 
 function delTodo(){
     event.target.parentNode.remove();
 }
-// function deleteAllTodo(){
-    // var ulList = document.getElementById("list")
-// }
+
+function editTodo(e){
+    var oldValue = e.parentNode.firstChild.nodeValue;
+    var newValue = prompt("Edit Todo", oldValue);
+
+    if(newValue !== null && newValue !== ""){
+        e.parentNode.firstChild.nodeValue = newValue;
+    }
+}
+
+function deleteAllTodo(){
+    var ulList = document.getElementById("list");
+    ulList.innerHTML = "";
+}
+
